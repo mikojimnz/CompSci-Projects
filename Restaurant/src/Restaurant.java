59e0a9ca074012f85b5847ce2d3964447d2a15bb
Miko <mikojimnz@users.noreply.github.com>
2016-12-02 09:33:28 -0500

    Create Restaurant.java

---
+++ Restaurant/src/Restaurant.java
@@ -0,0 +1,43 @@
+/**
+ *  Restaurant Midterm Project
+ *  <p>by mikojimnz</p>
+ * @author  mikojimnz
+ * @version 1.0
+ * @since   2016-12-02
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+class Restaurant extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	private JTextField cost, step;
+	private static int screenProgression = 0;
+	private double total, taxed;
+	private ImageIcon food1, food2, food3, side1, side2, side3, drink1, drink2,
+			drink3;
+
+	/**
+	 * Begin Restaurant
+	 * 
+	 * <p> Determines restaurant progression and draws GUIs </p>
+	 */
+	public static void main(String[] args) {
+		Restaurant frame = new Restaurant();
+		frame.setSize(800, 600);
+		frame.createGUI();
+		frame.setVisible(true);
+	}
+
+	private void createGUI() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Container window = getContentPane();
+
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		Object button = event.getSource();
+	}
+}
