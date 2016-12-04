513c38b9cc27b417048070ec70a65c71b81034e7
Miko <mikojimnz@users.noreply.github.com>
2016-12-03 21:12:44 -0500

    Update MixIt.java

--- MixIt/src/MixIt.java
+++ MixIt/src/MixIt.java
@@ -1,109 +1,232 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.Font;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-/**
- * <h1>Restaurant Midterm Project</h1>
- * 
- * @author mikojimnz
- * @version 1.0
- */
-
-class Restaurant extends JFrame implements ActionListener {
+public class MixIt extends JFrame implements ItemListener, ActionListener {
 	private static final long serialVersionUID = 1L;
-	private JTextField cost, step;
-	private JPanel welcome, frameFood, frameSides, frameDrinks, frameTotal, header, footer, bannerTotal, bannerMisc;
-	private static int screenProgression = 0;
-	private double total, taxed;
-	private static ImageIcon logo, food1, food2, food3, side1, side2, side3, drink1, drink2, drink3;
+	private JPanel panel, btns;
+	private JButton randBtn;
+	private static ImageIcon h1, h2, h3, h4, h5, b1, b2, b3, b4, b5, l1, l2, l3, l4, l5;
+	private Choice headChoice;
+	private int state = 0, head = 0, body = 0, legs = 0;
 
-	/**
-	 * Begin Restaurant
-	 * 
-	 * @param args
-	 *            Starting arguments
-	 */
 	public static void main(String[] args) {
-		Restaurant frame = new Restaurant();
-		frame.setSize(800, 600);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.createGUI();
-		frame.setVisible(true);
-
+		MixIt myMixIt = new MixIt();
+		myMixIt.setSize(750, 650);
+		myMixIt.createGUI();
+		myMixIt.setVisible(true);
 		
+		h1 = new ImageIcon("h1.png");
+		h2 = new ImageIcon("h2.png");
+		h3 = new ImageIcon("h3.png");
+		h4 = new ImageIcon("h4.png");
+		h5 = new ImageIcon("h5.png");
+		b1 = new ImageIcon("b1.png");
+		b2 = new ImageIcon("b2.png");
+		b3 = new ImageIcon("b3.png");
+		b4 = new ImageIcon("b4.png");
+		b5 = new ImageIcon("b5.png");
+		l1 = new ImageIcon("l1.png");
+		l2 = new ImageIcon("l2.png");
+		l3 = new ImageIcon("l3.png");
+		l4 = new ImageIcon("l4.png");
+		l5 = new ImageIcon("l5.png");
 	}
 
-	/**
-	 * Creates GUI
-	 */
 	private void createGUI() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		Container window = getContentPane();
+		window.setLayout(new BoxLayout(window, BoxLayout.Y_AXIS));
 
-		window.setLayout(new BorderLayout());
-		window.setBackground(new Color(255, 215, 145));
+		panel = new JPanel();
+		btns = new JPanel();
+		headChoice = new Choice();
+		randBtn = new JButton("Randomize: ");
 
-		/**
-		 * welcome - Inital Screen frameFood - main entrees frameSides - sides
-		 * frameDrinks - drinks frameTotal - checkput screen header - top banner
-		 * footer - bottom banner bannerTotal - left banner bannerMisc - right
-		 * banner
-		 */
+		panel.setPreferredSize(new Dimension(getWidth(), 550));
+		panel.setBackground(Color.DARK_GRAY);
+		btns.setPreferredSize(new Dimension(getWidth(), 50));
+		btns.setBackground(Color.WHITE);
+		btns.setLayout(new FlowLayout());
+		headChoice.add("Head");
+		headChoice.add("Body");
+		headChoice.add("Legs");
+		headChoice.add("All");
 
-		welcome = new JPanel();
-		welcome.setPreferredSize(new Dimension(getWidth(), 100));
-		welcome.setBackground(new Color(255, 215, 145));
+		headChoice.addItemListener(this);
+		randBtn.addActionListener(this);
 
-		frameFood = new JPanel();
-		frameFood.setBackground(new Color(255, 215, 145));
+		btns.add(randBtn);
+		btns.add(headChoice);
 
-		frameSides = new JPanel();
-		frameSides.setBackground(new Color(255, 215, 145));
-
-		frameDrinks = new JPanel();
-		frameDrinks.setBackground(new Color(255, 215, 145));
-
-		frameTotal = new JPanel();
-		frameTotal.setBackground(new Color(255, 215, 145));
-
-		header = new JPanel();
-		header.setPreferredSize(new Dimension(getWidth(), 100));
-		header.setBackground(new Color(255, 215, 145));
-
-		footer = new JPanel();
-		footer.setPreferredSize(new Dimension(getWidth(), 100));
-		footer.setBackground(new Color(255, 215, 145));
-
-		bannerTotal = new JPanel();
-		bannerTotal.setPreferredSize(new Dimension(300, getHeight()));
-		bannerTotal.setBackground(new Color(255, 215, 145));
-
-		bannerMisc = new JPanel();
-		bannerMisc.setPreferredSize(new Dimension(300, getHeight()));
-		bannerMisc.setBackground(new Color(255, 215, 145));
-
-		window.add(welcome, BorderLayout.CENTER);
-		window.add(header, BorderLayout.NORTH);
-		window.add(footer, BorderLayout.SOUTH);
-		window.add(bannerTotal, BorderLayout.WEST);
-		window.add(bannerMisc, BorderLayout.EAST);
+		window.add(panel);
+		window.add(btns);
 	}
 
-	/**
-	 * Called when button actions fire
-	 * 
-	 * @param event
-	 *            Passes along source of event.
-	 */
 	public void actionPerformed(ActionEvent event) {
-		Object button = event.getSource();
+		Graphics g = panel.getGraphics();
+		Object source = event.getSource();
+
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		if (source == randBtn) {
+			switch(state) {
+				case 0:
+					randomHead();
+					break;
+				case 1:
+					randomBody();
+					break;
+				case 2:
+					randomLegs();
+					break;
+				default:
+				case 3:
+					randomHead();
+					randomBody();
+					randomLegs();
+					break;
+			}
+		}
+		
+		preformDraw();
+
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getSource() == headChoice) {
+			String headString = e.getItem().toString();
+
+			if (headString.equals("Head")) {
+				state = 0;
+			}
+
+			if (headString.equals("Body")) {
+				state = 1;
+			}
+
+			if (headString.equals("Legs")) {
+				state = 2;
+			}
+
+			if (headString.equals("All")) {
+				state = 3;
+			}
+			
+			preformDraw();
+		}
+	}
+	
+	private void preformDraw() {
+		Graphics g = panel.getGraphics();
+		Font text = new Font("SansSerif", Font.BOLD, 24);
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		if (head == 0) h1.paintIcon(this, g, getWidth()/2-100, 10);
+		if (head == 1) h2.paintIcon(this, g, getWidth()/2-100, 10);
+		if (head == 2) h3.paintIcon(this, g, getWidth()/2-100, 10);
+		if (head == 3) h4.paintIcon(this, g, getWidth()/2-100, 10);
+		if (head == 4) h5.paintIcon(this, g, getWidth()/2-100, 10);
+		
+		if (body == 0) b1.paintIcon(this, g, getWidth()/2-100, 90);
+		if (body == 1) b2.paintIcon(this, g, getWidth()/2-100, 90);
+		if (body == 2) b3.paintIcon(this, g, getWidth()/2-100, 90);
+		if (body == 3) b4.paintIcon(this, g, getWidth()/2-100, 90);
+		if (body == 4) b5.paintIcon(this, g, getWidth()/2-100, 90);
+		
+		if (legs == 0) l1.paintIcon(this, g, getWidth()/2-100, 170);
+		if (legs == 1) l2.paintIcon(this, g, getWidth()/2-100, 170);
+		if (legs == 2) l3.paintIcon(this, g, getWidth()/2-100, 170);
+		if (legs == 3) l4.paintIcon(this, g, getWidth()/2-100, 170);
+		if (legs == 4) l5.paintIcon(this, g, getWidth()/2-100, 170);
+
+		g.setFont(text);
+		g.setColor(Color.white);
+		g.drawString(" Here is your head", 400, 30);
+		g.drawString(" Here is your body", 400, 100);
+		g.drawString(" Here is your legs", 400, 180);
+	}
+
+	public void randomHead() {
+		Graphics g = panel.getGraphics();		
+		head = (int) (Math.random() * 4);
+		System.out.println("random head = " + head);
+
+		for (int i = 0; i < 30; i++) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
+			if (i % 2 == 0) h1.paintIcon(this, g, getWidth()/2-100, 10);
+			if (i % 3 == 0) h2.paintIcon(this, g, getWidth()/2-100, 10);
+			if (i % 4 == 0) h3.paintIcon(this, g, getWidth()/2-100, 10);
+			if (i % 5 == 0) {
+				h4.paintIcon(this, g, getWidth()/2-100, 10);
+			} else {
+				h5.paintIcon(this, g, getWidth()/2-100, 10);
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	public void randomBody() {
+		Graphics g = panel.getGraphics();
+		body = (int) (Math.random() * 4);
+		System.out.println("random body = " + body);
+		
+		for (int i = 0; i < 30; i++) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
+			if (i % 2 == 0) b1.paintIcon(this, g, getWidth()/2-100, 90);
+			if (i % 3 == 0) b2.paintIcon(this, g, getWidth()/2-100, 90);
+			if (i % 4 == 0) b3.paintIcon(this, g, getWidth()/2-100, 90);
+			if (i % 5 == 0) {
+				b4.paintIcon(this, g, getWidth()/2-100, 90);
+			} else {
+				b5.paintIcon(this, g, getWidth()/2-100, 90);
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void randomLegs() {
+		Graphics g = panel.getGraphics();
+		legs = (int) (Math.random() * 4);
+		System.out.println("random legs = " + legs);
+		
+		for (int i = 0; i < 30; i++) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(0, 0, getWidth(), getHeight());
+
+			if (i % 2 == 0) l1.paintIcon(this, g, getWidth()/2-100, 170);
+			if (i % 3 == 0) l2.paintIcon(this, g, getWidth()/2-100, 170);
+			if (i % 4 == 0) l3.paintIcon(this, g, getWidth()/2-100, 170);
+			if (i % 5 == 0) {
+				l4.paintIcon(this, g, getWidth()/2-100, 170);
+			} else {
+				l5.paintIcon(this, g, getWidth()/2-100, 170);
+			}
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}		
 	}
 }
