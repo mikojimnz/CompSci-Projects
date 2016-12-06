14b2ad90943e5a6c0fd17b05b1e2335f44273a35
Miko <mikojimnz@users.noreply.github.com>
2016-12-06 14:37:04 -0500

    Update Restaurant.java

--- Restaurant/src/Restaurant.java
+++ Restaurant/src/Restaurant.java
@@ -12,17 +12,21 @@ import java.awt.Font;
 public class Restaurant extends JFrame implements ActionListener {
 	/** Initiates Variables */
 	private static final long serialVersionUID = 1L;
-	private JButton start, next, back, cancel, sandwhichAdd, saladAdd, grilledCheeseAdd, bageAdd, pastryAdd, soupAdd,
-			sodaAdd, latteAdd, smoothieAdd, sandwhichSub, saladSub, grilledCheeseSub, bageSubl, pastrySub, soupSub,
-			sodaSub, latteSub, smoothieSub;
-	private JLabel author, cost, tax, step, title, costTitle, taxTitle, sandwhichName, saladName, grilledCheeseName,
-			bagelName, pastryName, soupName, sodaName, latteName, smoothieName, sandwhichNum, saladNum,
-			grilledCheeseNum, bagelNum, pastryNum, soupNum, sodaNum, latteNum, smoothieNum;
-	private JPanel center, welcome, frameFood, frameSides, frameDrinks, frameTotal, header, footer, bannerTotal,
-			bannerOrder;
+	private JButton start, next, back, cancel, sandwhichAdd, saladAdd,
+			grilledCheeseAdd, bagelAdd, pastryAdd, soupAdd, sodaAdd, latteAdd,
+			smoothieAdd, sandwhichSub, saladSub, grilledCheeseSub, bagelSub,
+			pastrySub, soupSub, sodaSub, latteSub, smoothieSub;
+	private JLabel author, cost, tax, step, title, costTitle, taxTitle,
+			sandwhichName, saladName, grilledCheeseName, bagelName, pastryName,
+			soupName, sodaName, latteName, smoothieName, sandwhichNum,
+			saladNum, grilledCheeseNum, bagelNum, pastryNum, soupNum, sodaNum,
+			latteNum, smoothieNum;
+	private JPanel center, welcome, frameFood, frameSides, frameDrinks,
+			frameTotal, header, footer, bannerTotal, bannerOrder;
 	private double total, taxed;
-	private int frame = 0;
-	private static ImageIcon sandwhich, salad, grilledCheese, bagel, pastry, soup, soda, latte, smoothie;
+	private int frame = 0, sandwhichOrder = 0, saladOrder = 0,
+			grilledCheeseOrder = 0, bagelOrder = 0, pastryOrder = 0,
+			soupOrder = 0, sodaOrder = 0, latteOrder = 0, smoothieOrder = 0;
 
 	/**
 	 * Begin Restaurant and define {@link ImageIcon ImageIcons}.
@@ -31,15 +35,6 @@ public class Restaurant extends JFrame implements ActionListener {
 	 *            Starting arguments.
 	 */
 	public static void main(String[] args) {
-		sandwhich = new ImageIcon("chipotle-chicken.jpg");
-		salad = new ImageIcon("garden-chicken-wonton.jpg");
-		grilledCheese = new ImageIcon("grilled-cheese.jpg");
-		bagel = new ImageIcon("bagel.jpg");
-		pastry = new ImageIcon("cherry-pastry.jpg");
-		soup = new ImageIcon("french-onion.jpg");
-		soda = new ImageIcon("pepsi.jpg");
-		latte = new ImageIcon("pumpkin-spice.jpg");
-		smoothie = new ImageIcon("superfrui-smoothie.jpg");
 
 		Restaurant frame = new Restaurant();
 		frame.setTitle("Panera Bread");
@@ -70,8 +65,8 @@ public class Restaurant extends JFrame implements ActionListener {
 		/** Initalize SWING components */
 		author = new JLabel("Programmed by Miko Jimenez");
 		back = new JButton("Go Back");
-		bageAdd = new JButton("+");
-		bageSubl = new JButton("-");
+		bagelAdd = new JButton("+");
+		bagelSub = new JButton("-");
 		bannerOrder = new JPanel();
 		bannerTotal = new JPanel();
 		cancel = new JButton("Cancel Order");
@@ -156,12 +151,27 @@ public class Restaurant extends JFrame implements ActionListener {
 		sandwhichSub.setBackground(new Color(150, 54, 32));
 		saladSub.setBackground(new Color(150, 54, 32));
 		grilledCheeseSub.setBackground(new Color(150, 54, 32));
+		bagelAdd.setBackground(new Color(92, 114, 50));
+		pastryAdd.setBackground(new Color(92, 114, 50));
+		soupAdd.setBackground(new Color(92, 114, 50));
+		bagelSub.setBackground(new Color(150, 54, 32));
+		pastrySub.setBackground(new Color(150, 54, 32));
+		soupSub.setBackground(new Color(150, 54, 32));
+		sodaAdd.setBackground(new Color(92, 114, 50));
+		latteAdd.setBackground(new Color(92, 114, 50));
+		smoothieAdd.setBackground(new Color(92, 114, 50));
+		sodaSub.setBackground(new Color(150, 54, 32));
+		latteSub.setBackground(new Color(150, 54, 32));
+		smoothieSub.setBackground(new Color(150, 54, 32));
 
 		/** Set LayoutManager of JPanels */
 		window.setBackground(new Color(255, 215, 145));
 		welcome.setLayout(new GridBagLayout());
 		center.setLayout(new CardLayout());
 		frameFood.setLayout(new GridBagLayout());
+		frameSides.setLayout(new GridBagLayout());
+		frameDrinks.setLayout(new GridBagLayout());
+		frameTotal.setLayout(new GridBagLayout());
 		footer.setLayout(new GridBagLayout());
 
 		/** Define welcome panel GridBagConstraint properties */
@@ -211,6 +221,18 @@ public class Restaurant extends JFrame implements ActionListener {
 		drinkGrid.insets = new Insets(2, 2, 2, 2);
 		drinkGrid.anchor = GridBagConstraints.CENTER;
 		drinkGrid.fill = GridBagConstraints.NONE;
+
+		/** Define frameTotal panel GridBagConstraint properties */
+		GridBagConstraints totalGrid = new GridBagConstraints();
+		totalGrid.gridx = 1;
+		totalGrid.gridy = 1;
+		totalGrid.gridwidth = 1;
+		totalGrid.gridheight = 1;
+		totalGrid.weightx = 50;
+		totalGrid.weighty = 50;
+		totalGrid.insets = new Insets(2, 2, 2, 2);
+		totalGrid.anchor = GridBagConstraints.CENTER;
+		totalGrid.fill = GridBagConstraints.NONE;
 
 		/** Define footer panel GridBagConstraint properties */
 		GridBagConstraints footerGrid = new GridBagConstraints();
@@ -307,6 +329,102 @@ public class Restaurant extends JFrame implements ActionListener {
 		grilledCheeseSub.setBorderPainted(false);
 		grilledCheeseSub.addActionListener(this);
 
+		/** Define bagelAdd button properties */
+		bagelAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		bagelAdd.setForeground(Color.white);
+		bagelAdd.setOpaque(true);
+		bagelAdd.setContentAreaFilled(true);
+		bagelAdd.setBorderPainted(false);
+		bagelAdd.addActionListener(this);
+
+		/** Define pastryAdd button properties */
+		pastryAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		pastryAdd.setForeground(Color.white);
+		pastryAdd.setOpaque(true);
+		pastryAdd.setContentAreaFilled(true);
+		pastryAdd.setBorderPainted(false);
+		pastryAdd.addActionListener(this);
+
+		/** Define soupAdd button properties */
+		soupAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		soupAdd.setForeground(Color.white);
+		soupAdd.setOpaque(true);
+		soupAdd.setContentAreaFilled(true);
+		soupAdd.setBorderPainted(false);
+		soupAdd.addActionListener(this);
+
+		/** Define bagelSub button properties */
+		bagelSub.setFont(new Font("Arial", Font.BOLD, 25));
+		bagelSub.setForeground(Color.white);
+		bagelSub.setOpaque(true);
+		bagelSub.setContentAreaFilled(true);
+		bagelSub.setBorderPainted(false);
+		bagelSub.addActionListener(this);
+
+		/** Define pastrySub button properties */
+		pastrySub.setFont(new Font("Arial", Font.BOLD, 25));
+		pastrySub.setForeground(Color.white);
+		pastrySub.setOpaque(true);
+		pastrySub.setContentAreaFilled(true);
+		pastrySub.setBorderPainted(false);
+		pastrySub.addActionListener(this);
+
+		/** Define soupSub button properties */
+		soupSub.setFont(new Font("Arial", Font.BOLD, 25));
+		soupSub.setForeground(Color.white);
+		soupSub.setOpaque(true);
+		soupSub.setContentAreaFilled(true);
+		soupSub.setBorderPainted(false);
+		soupSub.addActionListener(this);
+
+		/** Define sodaAdd button properties */
+		sodaAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		sodaAdd.setForeground(Color.white);
+		sodaAdd.setOpaque(true);
+		sodaAdd.setContentAreaFilled(true);
+		sodaAdd.setBorderPainted(false);
+		sodaAdd.addActionListener(this);
+
+		/** Define latteAdd button properties */
+		latteAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		latteAdd.setForeground(Color.white);
+		latteAdd.setOpaque(true);
+		latteAdd.setContentAreaFilled(true);
+		latteAdd.setBorderPainted(false);
+		latteAdd.addActionListener(this);
+
+		/** Define smoothieAdd button properties */
+		smoothieAdd.setFont(new Font("Arial", Font.BOLD, 25));
+		smoothieAdd.setForeground(Color.white);
+		smoothieAdd.setOpaque(true);
+		smoothieAdd.setContentAreaFilled(true);
+		smoothieAdd.setBorderPainted(false);
+		smoothieAdd.addActionListener(this);
+
+		/** Define sodaSub button properties */
+		sodaSub.setFont(new Font("Arial", Font.BOLD, 25));
+		sodaSub.setForeground(Color.white);
+		sodaSub.setOpaque(true);
+		sodaSub.setContentAreaFilled(true);
+		sodaSub.setBorderPainted(false);
+		sodaSub.addActionListener(this);
+
+		/** Define latteSub button properties */
+		latteSub.setFont(new Font("Arial", Font.BOLD, 25));
+		latteSub.setForeground(Color.white);
+		latteSub.setOpaque(true);
+		latteSub.setContentAreaFilled(true);
+		latteSub.setBorderPainted(false);
+		latteSub.addActionListener(this);
+
+		/** Define smoothieSub button properties */
+		smoothieSub.setFont(new Font("Arial", Font.BOLD, 25));
+		smoothieSub.setForeground(Color.white);
+		smoothieSub.setOpaque(true);
+		smoothieSub.setContentAreaFilled(true);
+		smoothieSub.setBorderPainted(false);
+		smoothieSub.addActionListener(this);
+
 		/** Define JLabel Fonts */
 		author.setFont(new Font("Arial", Font.BOLD, 20));
 		title.setFont(new Font("Arial", Font.BOLD, 35));
@@ -334,18 +452,21 @@ public class Restaurant extends JFrame implements ActionListener {
 		latteNum.setFont(new Font("Arial", Font.BOLD, 25));
 		smoothieNum.setFont(new Font("Arial", Font.BOLD, 25));
 
-		/** Begin adding compnents to the JPanels */
+		/** Begin adding components to the JPanels */
 		welcome.add(new JLabel(new ImageIcon("logo.png")), welcomeGrid);
 		welcomeGrid.gridy = 2;
 		welcome.add(author, welcomeGrid);
 		welcomeGrid.gridy = 3;
 		welcome.add(start, welcomeGrid);
 
-		frameFood.add(new JLabel(new ImageIcon("chipotle-chicken.jpg")), foodGrid);
+		frameFood.add(new JLabel(new ImageIcon("chipotle-chicken.jpg")),
+				foodGrid);
 		foodGrid.gridx = 2;
-		frameFood.add(new JLabel(new ImageIcon("garden-chicken-wonton.jpg")), foodGrid);
+		frameFood.add(new JLabel(new ImageIcon("garden-chicken-wonton.jpg")),
+				foodGrid);
 		foodGrid.gridx = 3;
-		frameFood.add(new JLabel(new ImageIcon("grilled-cheese.jpg")), foodGrid);
+		frameFood
+				.add(new JLabel(new ImageIcon("grilled-cheese.jpg")), foodGrid);
 		foodGrid.gridx = 1;
 		foodGrid.gridy = 2;
 		frameFood.add(sandwhichName, foodGrid);
@@ -374,6 +495,78 @@ public class Restaurant extends JFrame implements ActionListener {
 		frameFood.add(saladSub, foodGrid);
 		foodGrid.gridx = 3;
 		frameFood.add(grilledCheeseSub, foodGrid);
+
+		frameSides.add(new JLabel(new ImageIcon("bagel.jpg")), sideGrid);
+		sideGrid.gridx = 2;
+		frameSides
+				.add(new JLabel(new ImageIcon("cherry-pastry.jpg")), sideGrid);
+		sideGrid.gridx = 3;
+		frameSides.add(new JLabel(new ImageIcon("grilled-cheese.jpg")),
+				sideGrid);
+		sideGrid.gridx = 1;
+		sideGrid.gridy = 2;
+		frameSides.add(bagelName, sideGrid);
+		sideGrid.gridx = 2;
+		frameSides.add(pastryName, sideGrid);
+		sideGrid.gridx = 3;
+		frameSides.add(soupName, sideGrid);
+		sideGrid.gridx = 1;
+		sideGrid.gridy = 3;
+		frameSides.add(bagelAdd, sideGrid);
+		sideGrid.gridx = 2;
+		frameSides.add(pastryAdd, sideGrid);
+		sideGrid.gridx = 3;
+		frameSides.add(soupAdd, sideGrid);
+		sideGrid.gridx = 1;
+		sideGrid.gridy = 4;
+		frameSides.add(bagelNum, sideGrid);
+		sideGrid.gridx = 2;
+		frameSides.add(pastryNum, sideGrid);
+		sideGrid.gridx = 3;
+		frameSides.add(soupNum, sideGrid);
+		sideGrid.gridx = 1;
+		sideGrid.gridy = 5;
+		frameSides.add(bagelSub, sideGrid);
+		sideGrid.gridx = 2;
+		frameSides.add(pastrySub, sideGrid);
+		sideGrid.gridx = 3;
+		frameSides.add(soupSub, sideGrid);
+
+		frameDrinks.add(new JLabel(new ImageIcon("pepsi.jpg")), drinkGrid);
+		drinkGrid.gridx = 2;
+		frameDrinks.add(new JLabel(new ImageIcon("pumpkin-spice.jpg")),
+				drinkGrid);
+		drinkGrid.gridx = 3;
+		frameDrinks.add(new JLabel(new ImageIcon("superfruit-smoothie.jpg")),
+				drinkGrid);
+		drinkGrid.gridx = 1;
+		drinkGrid.gridy = 2;
+		frameDrinks.add(sodaName, drinkGrid);
+		drinkGrid.gridx = 2;
+		frameDrinks.add(latteName, drinkGrid);
+		drinkGrid.gridx = 3;
+		frameDrinks.add(smoothieName, drinkGrid);
+		drinkGrid.gridx = 1;
+		drinkGrid.gridy = 3;
+		frameDrinks.add(sodaAdd, drinkGrid);
+		drinkGrid.gridx = 2;
+		frameDrinks.add(latteAdd, drinkGrid);
+		drinkGrid.gridx = 3;
+		frameDrinks.add(smoothieAdd, drinkGrid);
+		drinkGrid.gridx = 1;
+		drinkGrid.gridy = 4;
+		frameDrinks.add(sodaNum, drinkGrid);
+		drinkGrid.gridx = 2;
+		frameDrinks.add(latteNum, drinkGrid);
+		drinkGrid.gridx = 3;
+		frameDrinks.add(smoothieNum, drinkGrid);
+		drinkGrid.gridx = 1;
+		drinkGrid.gridy = 5;
+		frameDrinks.add(sodaSub, drinkGrid);
+		drinkGrid.gridx = 2;
+		frameDrinks.add(latteSub, drinkGrid);
+		drinkGrid.gridx = 3;
+		frameDrinks.add(smoothieSub, drinkGrid);
 
 		header.add(title);
 
@@ -425,84 +618,118 @@ public class Restaurant extends JFrame implements ActionListener {
 			title.setText("Choose Your Entrées");
 			costTitle.setText("Your Total Is:");
 			cost.setText("$" + total);
-			taxTitle.setText("With Tax");
-			tax.setText("$" + taxed);
 			step.setText(" Step 1 of 4 ");
 			next.setVisible(true);
 			back.setVisible(true);
 			cancel.setVisible(true);
 			frame++;
+			sandwhichOrder = 0;
+			saladOrder = 0;
+			grilledCheeseOrder = 0;
+			bagelOrder = 0;
+			pastryOrder = 0;
+			soupOrder = 0;
+			sodaOrder = 0;
+			latteOrder = 0;
+			smoothieOrder = 0;
+			sandwhichNum.setText("0");
+			saladNum.setText("0");
+			grilledCheeseNum.setText("0");
+			bagelNum.setText("0");
+			pastryNum.setText("0");
+			soupNum.setText("0");
+			sodaNum.setText("0");
+			latteNum.setText("0");
+			smoothieNum.setText("0");
 		}
 
 		if (e.getSource() == next) {
 			switch (frame) {
-			case 1:
-				cardLayout.next(center);
-				frame++;
-				title.setText("Choose Your Sides");
-				step.setText(" Step 2 of 4 ");
-				break;
-			case 2:
-				cardLayout.next(center);
-				frame++;
-				title.setText("Choose Your Drinks");
-				step.setText(" Step 3 of 4 ");
-				break;
-			case 3:
-				cardLayout.next(center);
-				frame++;
-				title.setText("Checkout");
-				step.setText(" Step 4 of 4 ");
-				next.setText("New Order");
-				break;
-			case 4:
-				cancel.doClick();
-				break;
+				case 1 :
+					cardLayout.next(center);
+					frame++;
+					title.setText("Choose Your Sides");
+					step.setText(" Step 2 of 4 ");
+					break;
+				case 2 :
+					cardLayout.next(center);
+					frame++;
+					title.setText("Choose Your Drinks");
+					step.setText(" Step 3 of 4 ");
+					break;
+				case 3 :
+					cardLayout.next(center);
+					frame++;
+					title.setText("Checkout");
+					step.setText(" Step 4 of 4 ");
+					next.setText("New Order");
+					break;
+				case 4 :
+					cancel.doClick();
+					break;
 			}
 		}
 
 		if (e.getSource() == back) {
 			switch (frame) {
-			case 0:
-				break;
-			case 1:
-				cardLayout.previous(center);
-				frame--;
-				title.setText("");
-				costTitle.setText("");
-				cost.setText("");
-				taxTitle.setText("");
-				tax.setText("");
-				step.setText("");
-				next.setVisible(false);
-				back.setVisible(false);
-				cancel.setVisible(false);
-				break;
-			case 2:
-				frame--;
-				cardLayout.previous(center);
-				title.setText("Choose Your Entrées");
-				step.setText(" Step 1 of 4 ");
-				break;
-			case 3:
-				frame--;
-				cardLayout.previous(center);
-				title.setText("Choose Your Sides");
-				step.setText(" Step 2 of 4 ");
-				break;
-			case 4:
-				frame--;
-				cardLayout.previous(center);
-				title.setText("Choose Your Drinks");
-				step.setText(" Step 3 of 4 ");
-				next.setText("Continue");
-				break;
+				case 0 :
+					break;
+				case 1 :
+					cardLayout.previous(center);
+					frame--;
+					title.setText("");
+					costTitle.setText("");
+					cost.setText("");
+					taxTitle.setText("");
+					tax.setText("");
+					step.setText("");
+					next.setVisible(false);
+					back.setVisible(false);
+					cancel.setVisible(false);
+					break;
+				case 2 :
+					frame--;
+					cardLayout.previous(center);
+					title.setText("Choose Your Entrées");
+					step.setText(" Step 1 of 4 ");
+					break;
+				case 3 :
+					frame--;
+					cardLayout.previous(center);
+					title.setText("Choose Your Sides");
+					step.setText(" Step 2 of 4 ");
+					break;
+				case 4 :
+					frame--;
+					cardLayout.previous(center);
+					title.setText("Choose Your Drinks");
+					step.setText(" Step 3 of 4 ");
+					next.setText("Continue");
+					break;
 			}
 		}
 
 		if (e.getSource() == cancel) {
 			cardLayout.first(center);
 			frame = 0;
+			sandwhichOrder = 0;
+			saladOrder = 0;
+			grilledCheeseOrder = 0;
+			bagelOrder = 0;
+			pastryOrder = 0;
+			soupOrder = 0;
+			sodaOrder = 0;
+			latteOrder = 0;
+			smoothieOrder = 0;
+			sandwhichNum.setText("0");
+			saladNum.setText("0");
+			grilledCheeseNum.setText("0");
+			bagelNum.setText("0");
+			pastryNum.setText("0");
+			soupNum.setText("0");
+			sodaNum.setText("0");
+			latteNum.setText("0");
+			smoothieNum.setText("0");
 			total = 0.0;
 			taxed = 0.0;
 			title.setText("");
@@ -514,6 +741,116 @@ public class Restaurant extends JFrame implements ActionListener {
 			next.setVisible(false);
 			back.setVisible(false);
 			cancel.setVisible(false);
+		}
+
+		if (e.getSource() == sandwhichAdd) {
+			sandwhichOrder++;
+			total += 6.99;
+			cost.setText("$" + cost);
+			sandwhichNum.setText(sandwhichOrder + "");
+		}
+
+		if (e.getSource() == sandwhichSub) {
+			if (sandwhichOrder != 0) {
+				sandwhichOrder--;
+				total -= 6.99;
+			}
+			cost.setText("$" + cost);
+			sandwhichNum.setText(sandwhichOrder + "");
+		}
+
+		if (e.getSource() == saladAdd) {
+			saladOrder++;
+			total += 10.07;
+			saladNum.setText(saladOrder + "");
+		}
+
+		if (e.getSource() == saladSub) {
+			if (saladOrder != 0) {
+				saladOrder--;
+				total -= 10.07;
+			}
+			saladNum.setText(saladOrder + "");
+		}
+
+		if (e.getSource() == grilledCheeseAdd) {
+			grilledCheeseOrder++;
+			total += 5.48;
+			grilledCheeseNum.setText(grilledCheeseOrder + "");
+		}
+
+		if (e.getSource() == grilledCheeseSub) {
+			if (grilledCheeseOrder != 0) {
+				grilledCheeseOrder--;
+				total -= 5.48;
+			}
+			grilledCheeseNum.setText(grilledCheeseOrder + "");
+		}
+
+		if (e.getSource() == bagelAdd) {
+			bagelOrder++;
+			bagelNum.setText(bagelOrder + "");
+		}
+
+		if (e.getSource() == bagelSub) {
+			if (bagelOrder != 0)
+				bagelOrder--;
+			bagelNum.setText(bagelOrder + "");
+		}
+
+		if (e.getSource() == pastryAdd) {
+			pastryOrder++;
+			pastryNum.setText(pastryOrder + "");
+		}
+
+		if (e.getSource() == pastrySub) {
+			if (pastryOrder != 0)
+				pastryOrder--;
+			pastryNum.setText(pastryOrder + "");
+		}
+
+		if (e.getSource() == soupAdd) {
+			soupOrder++;
+			soupNum.setText(soupOrder + "");
+		}
+
+		if (e.getSource() == soupSub) {
+			if (soupOrder != 0)
+				soupOrder--;
+			soupNum.setText(soupOrder + "");
+		}
+
+		if (e.getSource() == sodaAdd) {
+			sodaOrder++;
+			sodaNum.setText(sodaOrder + "");
+		}
+
+		if (e.getSource() == sodaSub) {
+			if (sodaOrder != 0)
+				sodaOrder--;
+			sodaNum.setText(sodaOrder + "");
+		}
+
+		if (e.getSource() == latteAdd) {
+			latteOrder++;
+			latteNum.setText(latteOrder + "");
+		}
+
+		if (e.getSource() == latteSub) {
+			if (latteOrder != 0)
+				latteOrder--;
+			latteNum.setText(latteOrder + "");
+		}
+
+		if (e.getSource() == smoothieAdd) {
+			smoothieOrder++;
+			smoothieNum.setText(smoothieOrder + "");
+		}
+
+		if (e.getSource() == smoothieSub) {
+			if (smoothieOrder != 0)
+				smoothieOrder--;
+			smoothieNum.setText(smoothieOrder + "");
 		}
 	}
 }
