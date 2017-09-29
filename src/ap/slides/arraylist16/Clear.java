b59af4d1da6e1e110d2ae5769356155ed4ff09a4
mikojiminez <mikojiminez@10.0.0.184>
2017-09-29 11:09:59 -0400

    Imported slides16

---
+++ src/ap/slides/arraylist16/Clear.java
@@ -0,0 +1,25 @@
+package ap.slides.arraylist16;
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//ArrayList clear() example
+
+import java.util.ArrayList;
+import static java.lang.System.*;
+
+public class Clear
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> ray;
+		ray = new ArrayList<String>();
+
+		ray.add("a");
+		ray.add("x");
+		ray.clear();
+		ray.add("t");
+		ray.add("w");
+
+		out.println(ray);
+	}
+}
---
+++ src/ap/slides/arraylist16/UserClassesOne.java
@@ -0,0 +1,35 @@
+package ap.slides.arraylist16;
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//ArrayList user defined classes example one
+
+import static java.lang.System.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class UserClassesOne
+{
+	public static void main ( String[] args )
+	{
+		ArrayList<Creature> creatureList;		//does this line have a problem
+
+		//add 3 creatures to creatureList
+
+
+
+
+
+
+
+		//add 3 lines to print out each Creature in creatureList
+
+
+
+
+
+
+
+
+  }
+}
