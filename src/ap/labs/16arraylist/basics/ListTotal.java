5a0d660422b7fbfc01cc2c21839768b7dc922def
mikojiminez <mikojiminez@10.0.0.184>
2017-09-29 10:51:14 -0400

    Rearagned files

---
+++ src/ap/labs/16arraylist/basics/ListTotal.java
@@ -0,0 +1,14 @@
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTotal {
+	public static int go(List<Integer> ray) {
+		return 0;
+	}
+}
