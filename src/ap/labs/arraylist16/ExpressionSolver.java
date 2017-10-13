1161b1a9600dfe65a69773cd1e628f6f5c3fb865
mikojiminez <jimenezmikog@gmail.com>
2017-10-13 11:16:23 -0400

    Push ExpressionSolver

--- src/ap/labs/arraylist16/ExpressionSolver.java
+++ src/ap/labs/arraylist16/ExpressionSolver.java
@@ -1,11 +1,13 @@
 package ap.labs.arraylist16;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ExpressionSolver {
 	private String expression = "";
-	private int temp;
-	private int in;
+	private ArrayList<Integer> nums = new ArrayList<Integer>();
+	private ArrayList<String> ops = new ArrayList<String>();
 	private int result;
-	private int operation;
 
 	public ExpressionSolver(String s) {
 		expression = s;
@@ -17,55 +19,58 @@ public class ExpressionSolver {
 	}
 
 	public void solveExpression() {
-		
-		for (int i = 0; i <= expression.length() - 1; i++) {
-			if (!Character.isWhitespace(expression.charAt(i))) {
-				if (Character.isDigit(expression.charAt(i)))  {
-					in = expression.charAt(i) - 48;
-					System.out.println(i + " | " + expression.charAt(i) + " | " + result + " = " + temp + " " + debug(operation) + " " + in );
-					calc();
+
+		for (int i = 0; i < expression.length(); i++) {
+			if (!Character.isWhitespace(expression.charAt(i)))
+				continue;
+
+			if (Character.isDigit(expression.charAt(i))) {
+				nums.add(expression.charAt(i) - 48);
+			} else {
+				switch (expression.charAt(i)) {
+				case '+':
+					ops.add("+");
+					break;
+				case '-':
+					ops.add("+");
+					break;
+				case '*':
+					ops.add("-");
+					break;
+				case '/':
+					ops.add("/");
+					break;
 				}
-				if (expression.charAt(i) == '+') operation = 0;
-				if (expression.charAt(i) == '-') operation = 1;
-				if (expression.charAt(i) == '*') operation = 2;
-				if (expression.charAt(i) == '/') operation = 3;
 			}
 		}
 
-	}
+		System.out.println(Arrays.asList(nums));
+		System.out.println(Arrays.asList(ops));
+		
+		for (int i = 0; i < ops.size(); i++) {
+			if (ops.get(i).equals("*")) {
+				nums.set(i, nums.get(i) * nums.get(i + 1));
+				nums.remove(i + 1);
+			} else {
+				nums.set(i, nums.get(i) / nums.get(i + 1));
+				nums.remove(i + 1);
+			}
 
-	private String debug(int a) {
-		switch (a) {
-		case 0: return "+";
-		case 1: return "-";
-		case 2: return "*";
-		case 3: return "/";
-		default: return "?";
-		}
-	}
-	
-	private void calc() {
-		
-		switch (operation) {
-		default:
-			break;
-		case 0:
-			temp = temp + in;
-			break;
-		case 1:
-			temp = temp - in;
-			break;
-		case 2:
-			temp = temp * in;
-			break;
-		case 3:
-			temp = temp / in;
-			break;
 		}
 		
-		result = temp;
-		temp = in;
+		for (int i = 0; i < ops.size(); i++) {
+			if (ops.get(i).equals("+")) {
+				nums.set(i, nums.get(i) + nums.get(i + 1));
+				nums.remove(i + 1);
+			} else {
+				nums.set(i, nums.get(i) - nums.get(i + 1));
+				nums.remove(i + 1);
+			}
+
+		}
 		
+	System.out.println(Arrays.asList(nums));
+
 	}
 
 	public String toString() {
