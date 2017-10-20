6670993b2a19127ca941d5e465ffefd9ab554520
mikojiminez <jimenezmikog@gmail.com>
2017-10-20 11:15:31 -0400

    Commit

--- src/ap/elevens/activity2/Deck.java
+++ src/ap/elevens/activity2/Deck.java
@@ -5,27 +5,11 @@ import java.util.ArrayList;
 public class Deck {
 
 	private ArrayList<Card> cards = new ArrayList<Card>();
-	private ArrayList<Card> dealtCards = new ArrayList<Card>();
-	private ArrayList<String> ranks = new ArrayList<String>();
-	private ArrayList<String> suits = new ArrayList<String>();
-	private ArrayList<Integer> values = new ArrayList<Integer>();
 	private int size;
 
-	public Deck(String[] ranks, String[] suits, int[] values) {
-		for (String i : ranks) {
-			this.ranks.add(i);
-		}
-
-		for (String i : suits) {
-			this.suits.add(i);
-		}
-
-		for (int i : values) {
-			this.values.add(i);
-		}
-
-		for (int i = 0; i < this.ranks.size(); i++) {
-			Card c = new Card(this.ranks.get(i), this.suits.get(i), this.values.get(i));
+	public Deck(String[] ranks, String[] suits, int[] values) {		
+		for (int i = 0; i < ranks.length; i++) {
+			Card c = new Card(ranks[i], suits[i], values[i]);
 			cards.add(c);
 		}
 
@@ -49,8 +33,7 @@ public class Deck {
 			return null;
 		} else {
 			this.size--;
-			Card c = this.cards.remove(this.size);
-			this.dealtCards.add(c);
+			Card c = this.cards.get(this.size);
 			return c;
 		}
 	}
@@ -71,12 +54,15 @@ public class Deck {
 		}
 
 		rtn = rtn + "\nDealt cards: \n";
-		for (int k = dealtCards.size() - 1; k >= size; k--) {
-			rtn = rtn + dealtCards.get(k);
-			if (k != size) {
+		
+		for (int i = cards.size() - 1; i >= size; i--) {
+			rtn = rtn + cards.get(i);
+			
+			if (i != size) {
 				rtn = rtn + ", ";
 			}
-			if ((k - dealtCards.size()) % 2 == 0) {
+			
+			if ((i - cards.size()) % 2 == 0) {
 				// Insert carriage returns so entire deck is visible on console.
 				rtn = rtn + "\n";
 			}
